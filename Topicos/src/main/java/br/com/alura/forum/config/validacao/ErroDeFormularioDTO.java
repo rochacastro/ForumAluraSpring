41a8a38a2d6140b42b214e15dd2f552e87bd5a0c
Vitor Rocha Castro Pereira <rochacastro.p@gmail.com>
2020-03-03 23:00:51 -0300

    primeiro commit

---
+++ Topicos/src/main/java/br/com/alura/forum/config/validacao/ErroDeFormularioDTO.java
@@ -0,0 +1,19 @@
+package br.com.alura.forum.config.validacao;
+
+public class ErroDeFormularioDTO {
+	private String campo;
+	private String erro;
+	
+	public String getCampo() {
+		return campo;
+	}
+	public String getErro() {
+		return erro;
+	}
+	
+	public ErroDeFormularioDTO(String campo, String erro) {
+		super();
+		this.campo = campo;
+		this.erro = erro;
+	}
+}
